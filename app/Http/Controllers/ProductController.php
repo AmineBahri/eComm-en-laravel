167c92349f9f2eda2b0e83b9a99604b8f9bd1f40
bahri <bahrimohamedamin7@gmail.com>
2020-11-13 00:56:49 +0100

    add to cart db and header done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Cart;
+use Session;
 
 class ProductController extends Controller
 {
@@ -25,5 +27,27 @@ class ProductController extends Controller
     {
         $data = Product::where('name', 'like', '%'.$request->input('query').'%')->get();
         return view('search', ['products' => $data]);
+    }
+
+    public function add_to_cart(Request $request)
+    {
+        if ($request->session()->has('user')) 
+        {
+            $cart = new Cart();
+            $cart->user_id = $request->session()->get('user')['id'];
+            $cart->product_id = $request->product_id;
+            $cart->save();
+            return redirect('/');
+        }
+        else
+        {
+            return redirect('/login');
+        }    
+    }
+
+    public static function cartItem()
+    {
+        $userId = Session::get('user')['id'];
+        return Cart::where('user_id',$userId)->count();
     }
 }
